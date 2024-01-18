f33103e0286d3ff67773472d9245a7b369f5e82f
rokkish <thehiro117@gmail.com>
2024-01-18 22:33:13 +0900

    cargo fmt

--- src/bin/1_6.rs
+++ src/bin/1_6.rs
@@ -10,7 +10,9 @@ enum Weapon {
 }
 
 fn can_equip_weapon(select_weapon: Weapon, have_sield: bool) -> bool {
-    if !have_sield { return true; }
+    if !have_sield {
+        return true;
+    }
     match select_weapon {
         Weapon::ZelcovaSword(_) => true,
         Weapon::CarrotSword(_) => true,
@@ -25,12 +27,27 @@ fn is_msg_can_not_equip_weapon(select_weapon: Weapon, have_sield: bool) -> bool 
     !can_equip_weapon(select_weapon, have_sield)
 }
 
-fn main () {
-    println!("{:?}", is_msg_can_not_equip_weapon(Weapon::ZelcovaSword(String::from("ZelcovaSword")), true));
-    println!("{:?}", is_msg_can_not_equip_weapon(Weapon::CarrotSword(String::from("CarrotSword")), true));
-    println!("{:?}", is_msg_can_not_equip_weapon(Weapon::WoodSword(String::from("WoodSword")), true));
-    println!("{:?}", is_msg_can_not_equip_weapon(Weapon::DragonSpear(String::from("DragonSpear")), true));
-    println!("{:?}", is_msg_can_not_equip_weapon(Weapon::KusanagiSword(String::from("KusanagiSword")), true));
+fn main() {
+    println!(
+        "{:?}",
+        is_msg_can_not_equip_weapon(Weapon::ZelcovaSword(String::from("ZelcovaSword")), true)
+    );
+    println!(
+        "{:?}",
+        is_msg_can_not_equip_weapon(Weapon::CarrotSword(String::from("CarrotSword")), true)
+    );
+    println!(
+        "{:?}",
+        is_msg_can_not_equip_weapon(Weapon::WoodSword(String::from("WoodSword")), true)
+    );
+    println!(
+        "{:?}",
+        is_msg_can_not_equip_weapon(Weapon::DragonSpear(String::from("DragonSpear")), true)
+    );
+    println!(
+        "{:?}",
+        is_msg_can_not_equip_weapon(Weapon::KusanagiSword(String::from("KusanagiSword")), true)
+    );
     println!("{:?}", is_msg_can_not_equip_weapon(Weapon::None, true));
 }
 
@@ -55,8 +72,15 @@ mod tests {
     // have sield && cant equip weapon
     #[case(Weapon::WoodSword(String::from("WoodSword")), true, true)]
     // #[case(Weapon::DragonSpear(String::from("DragonSpear")), true, true)]
-    fn test_equip_sield(#[case] select_weapon: Weapon, #[case] have_sield: bool, #[case] expected: bool) {
-        assert_eq!(is_msg_can_not_equip_weapon(select_weapon, have_sield), expected);
+    fn test_equip_sield(
+        #[case] select_weapon: Weapon,
+        #[case] have_sield: bool,
+        #[case] expected: bool,
+    ) {
+        assert_eq!(
+            is_msg_can_not_equip_weapon(select_weapon, have_sield),
+            expected
+        );
     }
     // 教訓：不要なテストケースを書かない
 }
