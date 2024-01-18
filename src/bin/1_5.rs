6d47d72c1df0986562c2f26a7ff024340540135d
rokkish <thehiro117@gmail.com>
2024-01-18 21:42:13 +0900

    Refactor solve function to return Option<i32> for age ranges

--- src/bin/1_5.rs
+++ src/bin/1_5.rs
@@ -1,17 +1,18 @@
 // ticket fee system
 
-fn solve(age: i32) -> i32 {
+fn solve(age: i32) -> Option<i32> {
     match age {
-        0..=5 => 0,
-        6..=12 => 500,
-        13..=17 => 1000,
-        18..=i32::MAX => 1500, // over 18
-        _ => 0,                // under 0 is 0
+        0..=5 => Some(0),
+        6..=12 => Some(500),
+        13..=17 => Some(1000),
+        18..=120 => Some(1500), // over 18
+        121..=i32::MAX => None, // over 120
+        _ => Some(0),           // under 0 is 0
     }
 }
 
 fn main() {
-    println!("{}", solve(5));
+    println!("{:?}", solve(5));
 }
 
 #[cfg(test)]
@@ -20,18 +21,23 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case(-1, 0)]
-    #[case(2, 0)]
-    #[case(5, 0)]
-    #[case(6, 500)]
-    #[case(8, 500)]
-    #[case(12, 500)]
-    #[case(13, 1000)]
-    #[case(15, 1000)]
-    #[case(17, 1000)]
-    #[case(18, 1500)]
-    #[case(20, 1500)]
-    fn test_solve(#[case] age: i32, #[case] expected: i32) {
+    #[case(-10, Some(0))] // missing case
+    #[case(-1, Some(0))]
+    #[case(0, Some(0))] // missing case
+    #[case(2, Some(0))]
+    #[case(5, Some(0))]
+    #[case(6, Some(500))]
+    #[case(8, Some(500))]
+    #[case(12, Some(500))]
+    #[case(13, Some(1000))]
+    #[case(15, Some(1000))]
+    #[case(17, Some(1000))]
+    #[case(18, Some(1500))]
+    #[case(20, Some(1500))]
+    #[case(120, Some(1500))] // missing case
+    #[case(121, None)] // missing case
+    #[case(140, None)] // missing case
+    fn test_solve(#[case] age: i32, #[case] expected: Option<i32>) {
         assert_eq!(solve(age), expected);
     }
 }
